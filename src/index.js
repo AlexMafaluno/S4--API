1c591e4fed7e3f316dc36dc1067b928984d31e7a
Alejandro Martinez <mart.perez93@gmail.com>
2025-01-03 11:05:59 +0100

    función llamar API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = hello;
-const world = 'world';
-function hello(who = world) {
-    return `Hi ${who}! `;
+
+const resultDiv = document.getElementById('result');
+const button = document.querySelector("button");
+const bromaContainer = document.querySelector('.result');
+
+
+function traerBroma() {
+    fetch('https://icanhazdadjoke.com/slack', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+        .then(res => {
+        if (!res.ok) {
+            throw new Error('Error en la resposta de l\'API');
+        }
+        return res.json();
+    })
+        .then(data => {
+        console.log(data);
+        displayBroma(data);
+    })
+        .catch(error => {
+        console.error('Hi ha hagut un error:', error);
+    });
 }
+function displayBroma(data) {
+    const chiste = data.attachments[0].text;
+    console.log(chiste);
+    resultDiv.innerHTML = chiste;
+}
+traerBroma();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,51 @@
-const world = 'world';
+"use strict";
 
-export function hello(who: string = world): string {
-  return `Hi ${who}! `;
+
+type JokeData = {
+  attachments: {
+      fallback: string;
+      footer: string;
+      text: string;
+  }[];
+  response_type: string;
+  username: string;
+};
+
+//usado el operador de aserció no nula !. Le digo a TS qie confie que el valor no es null
+const resultDiv = document.getElementById('result')!;
+const button = document.querySelector("button");
+const bromaContainer = document.querySelector('.result');
+
+
+
+function traerBroma(){
+  fetch('https://icanhazdadjoke.com/slack', {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json'
+    }
+  })
+  .then(res => {
+    if (!res.ok) {
+      throw new Error('Error en la resposta de l\'API');
+  }
+  return res.json();
+  })
+  .then(data => {
+    console.log(data);
+    displayBroma(data);
+  })
+  .catch(error => {
+    console.error('Hi ha hagut un error:', error);
+});
+
 }
+
+function displayBroma(data : JokeData) : void {
+  
+  const chiste = data.attachments[0].text;
+  console.log(chiste);
+  resultDiv.innerHTML = chiste;
+
+  }
+  traerBroma();
