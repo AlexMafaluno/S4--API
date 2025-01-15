35db630d624e47908e3ee554773a861d72ada25c
Alejandro Martinez <mart.perez93@gmail.com>
2025-01-15 17:21:39 +0100

    Optimizar código

--- src/index.ts
+++ src/index.ts
@@ -40,90 +40,66 @@ type ChuckNorrisJoke = {
   value: string;
 };
 
-
-//usado el operador de aserció no nula !. Le digo a TS qie confie que el valor no es null
 const resultDiv = document.getElementById('result')!;
 const button = document.getElementById("otra-broma")!;
-const bromaContainer = document.querySelector('.result');
-let jokeId = 1;
-let score = 0;
+let jokeId: number = 1;
+let score: number = 0;
 const reportAcudits : ReportTypes [] = [];
 
-
-//validación 
 
 if (!resultDiv || !button) {
   throw new Error("Los elementos del DOM no se encontraron correctamente.");
 }
 
-// llama a ua función que traerá una broma o otra
-// maejo de promesas correctamente
-async function alternarBroma() {
-  let random = Math.random();
-  console.log("Broma alternada: ", random);
-  random > 0.5 ? await traerBroma() : await traerChuck();
+
+async function alternarBroma(): Promise<void> {
+  const random = Math.random();
+  const url = random > 0.5 ? url2 : url1;
+  await traerBromaRandom(url);
 };
 
-function traerChuck(): Promise<ChuckNorrisJoke> {
+const url1: string = 'https://api.chucknorris.io/jokes/random';
+const url2: string = 'https://icanhazdadjoke.com/slack';
+const urlTiempo: string = 'https://api.openweathermap.org/data/2.5/weather?q=Barcelona,es&appid=6c67d66569fb8dd088c593f9164f514b&units=metric&lang=es';
 
-  return fetch('https://api.chucknorris.io/jokes/random', {
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json'
-    },
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Error en la resposta de l\'API');
-    }  
-    return response.json();
-  })
-  .then((data) => {
-    const chiste : string = data.value;
-    console.log("Broma obtenida:", chiste);
-    displayBroma(chiste);
-    crearAcudit(jokeId, chiste, score);  // push al array
-    return data;
-  })
-  .catch((error) => {
-    console.error('Hi ha hagut un error:', error);
-    throw error;
-  });
-}
+function traerBromaRandom(url: string): Promise<JokeData> {
+ return fetch(url, {
+  method: 'GET',
+  headers: {
+      'Accept': 'application/json'
+  },
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Error en la resposta de l\'API');
+  }  
+  return response.json();
+})
+.then((data) => {
+ 
+  let chiste : string;
 
-function traerBroma(): Promise<JokeData> {
-  
-  return fetch('https://icanhazdadjoke.com/slack', {
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json'
-    },
-  })
+  if(url === url2){ chiste = data.attachments[0].text;
+  }else if(url === url1){
+    chiste = data.value;
+  }else {
+    throw new Error('Fuente de datos no reconocida');
+  }
 
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Error en la resposta de l\'API');
-    }  
-    return response.json();
-  })
-
-  .then((data) => {
-    console.log("Broma obtenida:", data);
-    const chiste : string = data.attachments[0].text; // cada vez que llama a la fució traer roma deeria traer una broma nueva y guardarla e chiste
-    displayBroma(chiste); // solo muestra e el dom
-    crearAcudit(jokeId, chiste, score);  // push al array
-    return data;
-  })
-  .catch((error) => {
-    console.error('Hi ha hagut un error:', error);
-    throw error;
-  });
-}
+  resultDiv.innerHTML = chiste;
+  crearAcudit(jokeId, chiste, score);  // push al array
+  return data;
+})
+.catch((error) => {
+  console.error('Hi ha hagut un error:', error);
+  throw error;
+});
+};
 
 async function traerTiempo() {
  
   try {
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Barcelona,es&appid=6c67d66569fb8dd088c593f9164f514b&units=metric&lang=es', {
+    const response = await fetch(urlTiempo, {
     method: 'GET',
     headers: {
         'Accept': 'application/json'
@@ -136,9 +112,9 @@ async function traerTiempo() {
   const data: WeatherData = await response.json();
   mostrarTiempo(data);
   console.log("Temperatura actual:", data.main.temp);
-        }catch (error) {
-            console.error('Error:', error);
-        }
+  }catch (error) {
+    console.error('Error:', error);
+  }
 };
 
 
@@ -148,17 +124,14 @@ function mostrarTiempo(data: any) {
   
   if (iconMeteo instanceof HTMLElement) {
     iconMeteo.innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="icono del clima">`;
-    // iconMeteo.innerHTML = <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="icono del clima">;
-    // `<i class="wi wi-owm-${data.weather[0].id}"></i>`;
-  }
-    else {
+  } else {
     console.error('Element with ID "iconMeteo" not found or is not a valid HTML element');
   }
+  
   if (info instanceof HTMLElement) {
     info.innerHTML = `${data.name}: ${data.weather[0].description} <br>
     ${parseInt(data.main.temp)}°C`;
-  }
-  else {
+  } else {
   console.error('Element with ID "infoMeteo" not found or is not a valid HTML element');
   }
 };
@@ -167,55 +140,31 @@ function main() {
   traerTiempo()
   alternarBroma()
 };
+main();
 
-//DISPLAYS
-
-function displayBroma(data : string) : void{
-  resultDiv.innerHTML = data;
-  };
-  
 
 // Función para crear un reporte de una broma
  function crearAcudit (id:number, joke: string, score: number ): void{
   
-
-    const alreadyAcudit = reportAcudits.find((acudit) => acudit.id === id);
-    console.log(alreadyAcudit);
+  const alreadyAcudit = reportAcudits.find((acudit) => acudit.id === id);
+  console.log(alreadyAcudit);
     
-    if (alreadyAcudit) return console.log("La broma ya ha sido reportada:", alreadyAcudit);
-  
-    
-    // sio la ecuetra la crea
-    const acudit: ReportTypes = {
-      id,
-      joke, 
-      score, 
-      date : new Date().toISOString(),
-    };
+  if (alreadyAcudit) return console.log("La broma ya ha sido reportada:", alreadyAcudit);
+  const acudit: ReportTypes = { id, joke, score, date : new Date().toISOString()};
 
-    // Guardar la broma en el array de reportes
   reportAcudits.push(acudit);
   console.log("Broma reportada:", acudit);
-  console.log("Array de reportes actualizado:", reportAcudits);
-  
+  console.log("Array de reportes actualizado:", reportAcudits);  
   jokeId++;
-  
-}
-
-//PUTUAR BROMA
+};
 
 function puntuarBroma(score: number): void {
   
-
-  // Encuentra la última broma en el array
   const bromaActual = reportAcudits[reportAcudits.length - 1];
-
   if (!bromaActual) {
     console.error("No hay ninguna broma para puntuar.");
     return;
   }
-
-  // Actualiza el score si es diferente
   if (bromaActual.score !== score) {
     bromaActual.score = score;
     console.log("El score ha cambiado a:", score);
@@ -227,5 +176,5 @@ function puntuarBroma(score: number): void {
 
 
 
-main();
+
 
